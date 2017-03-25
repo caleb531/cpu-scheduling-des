4bb222c60ae0f5250a5ebb0f67b5dface6e6bc0a
Caleb Evans <caleb@calebevans.me>
2017-03-24 17:10:05 -0700

    Move core scheduler logic back to Scheduler class
    This change is an attempt to keep as much of the scheduler logic in the
    Scheduler class as possible, with the GenericScheduler class only
    defining the minimum necessary for it to function.

--- GenericScheduler.h
+++ GenericScheduler.h
@@ -2,33 +2,13 @@
  * Scheduler abstract base class definition
  */
 #pragma once
-#include <deque>
-#include <queue>
-#include "GenericScheduler.h"
-#include "Process.h"
 #include "Event.h"
 using namespace std;
-
-// Define handy aliases to complex queue types used throughout program
-typedef queue<Process*> ProcessQueue;
-typedef priority_queue<Process*, deque<Process*>, ProcessComparator> ProcessPriorityQueue;
-typedef priority_queue<Event*, deque<Event*>, EventComparator> EventPriorityQueue;
 
 // A generic Scheduler class that does not use templates; this class is useful
 // for declaring Scheduler pointers without knowing their template type in
 // advance
 class GenericScheduler {
-
-	protected:
-
-		// The list of all processes in the system
-		vector<Process*> procTable;
-		// A reference to the process event queue; this is supplied when the
-		// scheduler is instantiated
-		EventPriorityQueue *eventQueue;
-
-		// Flag to note whether the CPU is being used or not
-		bool isCPUIdle;
 
 	public:
 
--- Scheduler.h
+++ Scheduler.h
@@ -2,10 +2,17 @@
  * Scheduler abstract base class definition
  */
 #pragma once
+#include <deque>
+#include <queue>
 #include "Process.h"
 #include "Event.h"
 #include "GenericScheduler.h"
 using namespace std;
+
+// Define handy aliases to complex queue types used throughout program
+typedef queue<Process*> ProcessQueue;
+typedef priority_queue<Process*, deque<Process*>, ProcessComparator> ProcessPriorityQueue;
+typedef priority_queue<Event*, deque<Event*>, EventComparator> EventPriorityQueue;
 
 // The Scheduler class should not be instantiated itself; it should instead be
 // subclassed, with the following virtual methods implemented
@@ -14,9 +21,17 @@ class Scheduler : public GenericScheduler {
 
 	protected:
 
+		// The list of all processes in the system
+		vector<Process*> procTable;
+		// A reference to the process event queue; this is supplied when the
+		// scheduler is instantiated
+		EventPriorityQueue *eventQueue;
 		// The queue containing processes in the READY state; each subclass
 		// defines the particular type of queue
 		ReadyQueue readyQueue;
+
+		// Flag to note whether the CPU is being used or not
+		bool isCPUIdle;
 
 	public:
 
